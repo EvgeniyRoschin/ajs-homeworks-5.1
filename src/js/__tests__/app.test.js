9ba1328ab3a1a1c20fa39a2fd97e24f46dca07d5
Evgeniy Roschin <evgeny689@yandex.ru>
2019-12-06 10:20:32 +0400

    correct test

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -8,10 +8,10 @@ test('Success found', () => {
 
   const result = [
     { key: 'name', value: 'мечник' },
-    { key: 'level', value: 2 },
-    { key: 'attack', value: 80 },
-    { key: 'defence', value: 40 },
-    { key: 'health', value: 10 },
+    { key: 'level', value: '2' },
+    { key: 'attack', value: '80' },
+    { key: 'defence', value: '40' },
+    { key: 'health', value: '10' },
   ];
 
   expect(example).toEqual(result);
